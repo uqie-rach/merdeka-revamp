c31276a1d516820b560e5773ce7abbe35dda8e00
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2026-03-15 21:56:56 +0700

    enhanceh(trending): fix color on dark mode

--- components/layout/Trending/Trending.tsx
+++ components/layout/Trending/Trending.tsx
@@ -21,7 +21,7 @@ const Trending = () => {
     return (
         <section className="mt-2 px-4 sm:px-6 lg:px-8">
             <div className="bg-primary max-w-7xl mx-auto rounded-md">
-                <div className="relative bg-purple-100 dark:bg-[#0a0a0a] rounded-md border-2 border-primary/80 w-full h-13.5 flex items-center py-1 px-3 overflow-hidden">
+                <div className="relative bg-purple-100 dark:bg-purple-950 rounded-md border-2 border-primary/80 w-full h-13.5 flex items-center py-1 px-3 overflow-hidden">
 
                     {/* Title */}
                     <span className="flex gap-2 items-center text-primary mr-4 shrink-0">
@@ -38,7 +38,7 @@ const Trending = () => {
                                 <a
                                     key={i}
                                     href={item.href}
-                                    className="flex items-center gap-1 px-4 py-1 text-xs md:text-sm dark:text-gray-300 rounded-full bg-primary/10 dark:bg-purple-900/40 shrink-0"
+                                    className="flex items-center gap-1 px-4 py-1 text-xs md:text-sm dark:text-gray-300 rounded-full bg-primary/10 dark:bg-purple-700 shrink-0"
                                 >
                                     {item.label}
 
@@ -50,10 +50,10 @@ const Trending = () => {
                         </div>
 
                         {/* left blur */}
-                        <div className="absolute left-0 top-0 h-full w-12 bg-linear-to-r from-purple-100 dark:from-[#0a0a0a] to-transparent pointer-events-none" />
+                        <div className="absolute left-0 top-0 h-full w-12 bg-linear-to-r from-purple-100 dark:from-purple-950 to-transparent pointer-events-none" />
 
                         {/* right blur */}
-                        <div className="absolute right-0 top-0 h-full w-12 bg-linear-to-r from-purple-100 dark:from-[#0a0a0a] to-transparent pointer-events-none" />
+                        <div className="absolute right-0 top-0 h-full w-12 bg-linear-to-l from-purple-100 dark:from-purple-950 to-transparent pointer-events-none" />
 
                     </div>
 
